9417209f87ecb679a7306f7f7f08a53617ea4c60
Mykyta Poturai <mykyta.poturai@gmail.com>
2026-05-19 21:39:54 +0300

    Improve build output
    Gather all errors and print them at the end so you don't have to scroll
    through the build output to find them.

--- src/highheat/shell.py
+++ src/highheat/shell.py
@@ -35,9 +35,9 @@ def trim_left(string: str, length: int) -> str:
     else:
         return "..." + string[-(length-3):]
 
-def direct_logs(cmdlin:str, master:int):
+def direct_logs(cmdlin:str, master:int, errmaster:int):
     while True:
-        ready, _, _ = select.select([master], [], [], 0.1)
+        ready, _, _ = select.select([master, errmaster], [], [], 0.1)
         if master in ready:
             try:
                 data = os.read(master, 1024).decode('utf-8')
@@ -45,38 +45,64 @@ def direct_logs(cmdlin:str, master:int):
                 break
             print(data,end="")
 
+        if errmaster in ready:
+            try:
+                data = os.read(errmaster, 1024).decode('utf-8')
+            except OSError:
+                break
+            print(data,end="")
 
-def status_logs(cmdline:str, master:int):
+
+def status_logs(cmdline:str, master:int, errmaster:int) -> str:
     terminal_width = shutil.get_terminal_size().columns
     filler = " " * terminal_width
     cursor.hide()
     h = halo.Halo(text=trim_left(cmdline, terminal_width - 5), spinner="dots")
+    errbuf = ""
+
     spinnertext = h.frame()
     end = ""
     while True:
-        ready, _, _ = select.select([master], [], [], 0.1)
-        if master in ready:
+        err = False
+        data = ""
+        ready, _, _ = select.select([master, errmaster], [], [], 0.1)
+        if is_new_frame(0.1):
+            spinnertext = h.frame()
+        if errmaster in ready:
             try:
-                data = os.read(master, 1024).decode('utf-8')
+                err = True
+                data += os.read(errmaster, 1024).decode('utf-8')
+                errbuf += data
             except OSError:
                 break
-            if is_new_frame(0.1):
-                spinnertext = h.frame()
+
+        if master in ready:
+            try:
+                data += os.read(master, 1024).decode('utf-8')
+            except OSError:
+                break
+
+        if data != "":
             print(filler + "\r", end="") # Clear previous spinner line
             if end == "\n":
-                print("\033[A", end="")
+                print("\033[A", end="") # Move up one line
             end = "" if data.endswith("\n") else "\n"
             print(data,end=end)
-            print(spinnertext + "\r", end="")
+            print(spinnertext, end="\r")
+        else:
+            print(spinnertext, end="\r")
+
 
     cursor.show()
     spinnertext = h.frame()
     print(filler + "\r") # Clear previous spinner line
     sys.stdout.flush()
+    return errbuf
 
 
 def run_cmd(command: str) -> bool:
     log_cmd(command)
+    errbuf = ""
     if config.conf.confirmcmd and not config.conf.dryrun:
         if input("Continue? [y/N]: ").lower() not in 'yY':
             return False
@@ -85,17 +111,29 @@ def run_cmd(command: str) -> bool:
         return True
     try:
         master, slave = pty.openpty()
-        result = subprocess.Popen(command, shell=True, text=True, stdout=slave, stderr=slave, bufsize=0)
+        errmaster, errslave = pty.openpty()
+        result = subprocess.Popen(command, shell=True, text=True, stdout=slave, stderr=errslave, bufsize=0)
         os.close(slave)
+        os.close(errslave)
         flags = fcntl.fcntl(master, fcntl.F_GETFL)
         fcntl.fcntl(master, fcntl.F_SETFL, flags | os.O_NONBLOCK)
 
-        if sys.stdout.isatty():
-            status_logs(command, master)
-        else:
-            direct_logs(command, master)
+        flags = fcntl.fcntl(errmaster, fcntl.F_GETFL)
+        fcntl.fcntl(errmaster, fcntl.F_SETFL, flags | os.O_NONBLOCK)
 
-        return result.wait() == 0
+        if sys.stdout.isatty():
+            errbuf = status_logs(command, master, errmaster)
+        else:
+            direct_logs(command, master, errmaster)
+
+        retcode = result.wait()
+        if retcode != 0:
+            logger.error("Command '%s' failed with code %d", command, retcode)
+            if errbuf:
+                logger.error("stderr:\n")
+                print(errbuf)
+        return retcode == 0
+
     except subprocess.CalledProcessError as e:
         logger.error("Command '%s' failed with error: %d", command, e.returncode)
         return False
